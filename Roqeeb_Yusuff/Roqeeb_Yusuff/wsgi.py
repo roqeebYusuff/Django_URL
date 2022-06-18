9a8bf33ad36e80c52508887b3b1d1b6ea1ad383c
roqeebYusuff <roqeebyusuff001@gmail.com>
2022-06-18 13:15:22 +0100

    First commit

---
+++ Roqeeb_Yusuff/Roqeeb_Yusuff/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Roqeeb_Yusuff project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Roqeeb_Yusuff.settings')
+
+application = get_wsgi_application()
